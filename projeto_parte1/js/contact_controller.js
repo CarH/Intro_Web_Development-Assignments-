cdf0a03b1c4760fb04e21fc6abb3d9596068b8ae
Alex Barboza <barboza@localhost.localdomain>
2015-04-23 12:22:27 -0300

    Validação e persistência. Ok

--- projeto_parte1/js/contact_controller.js
+++ projeto_parte1/js/contact_controller.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
 	$("#enviarbtn").click(function() {
 		var new_contact = new Contact();
 		new_contact.setUserName($("#tfnome").val());
-		new_contact.setUserMail($("#user_mail").val());
+		new_contact.setUserMail($("#tfemail").val());
 		new_contact.setUserPhone($("#user_phone").val());
 		var counter = 0;
 		$("input:checkbox[name=comonosconheceu]").each(function() {
