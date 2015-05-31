3114a4a430f74c5f52a7eed8806ed8c230eed206
CarH <carlosh.usp@gmail.com>
2015-05-31 20:06:33 -0300

    criacao da pag login(especificacao)

---
+++ projeto_parte2/singaporeHotel/src/main/java/servlets/ServletHotel.java
@@ -0,0 +1,134 @@
+package servlets;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class ServletHotel
+ */
+public class ServletHotel extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	private static final boolean DEBUG = true;
+	
+	private ServletContext context;
+	private final String USUARIOS_CADASTRADOS = "usuariosCadastrados";
+	
+	
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public ServletHotel() {
+        super();
+    }
+    
+    @Override
+    public void init() throws ServletException {
+       	super.init();
+       	this.context = getServletConfig().getServletContext();
+		this.context.setAttribute(this.USUARIOS_CADASTRADOS, new ArrayList<Usuario>());
+    }
+    
+    
+    /**************************************************************
+     * Metodos relacionados com a lógica de CONTROLE da aplicacao
+     * 
+     */
+    
+    /**
+     * TODO: usuario ja cadastrado(so na fase 3, obviamente c/ BD)
+     * @param request
+     * @return
+     */
+    private boolean cadastrarUsuario(HttpServletRequest request){
+		/* Recuperando o contexto da aplicacao */
+		ServletContext context = getServletConfig().getServletContext();
+		ArrayList<Usuario> uCadastrados = (ArrayList<Usuario>) context.getAttribute(this.USUARIOS_CADASTRADOS);
+		
+		Usuario usuario = new Usuario();
+		usuario.setNome(request.getParameter("nome"));
+		usuario.setCpf(request.getParameter("cpf"));
+		usuario.setDataNasc(request.getParameter("datanasc"));
+		usuario.setSexo(request.getParameter("sexo"));
+		usuario.setEstadoCivil(request.getParameter("estadocivil"));
+		usuario.setCidade(request.getParameter("cidade"));
+		usuario.setEstado(request.getParameter("estado"));
+		usuario.setCep(request.getParameter("cep"));
+		usuario.setEmail(request.getParameter("email"));
+		usuario.setSenha(request.getParameter("senha"));
+		
+		uCadastrados.add(usuario);
+		
+		/* Atualiza no contexto da aplicacao */
+		context.setAttribute(this.USUARIOS_CADASTRADOS, uCadastrados);
+		
+		if (ServletHotel.DEBUG) { 
+			for (Usuario u : uCadastrados) {
+				System.out.println("Usuario "+u.getNome());
+			}
+			
+			System.out.println("usuario "+uCadastrados.get(uCadastrados.size()-1).getNome()+ " inserido com sucesso!");
+		}
+		return true;
+    }
+    
+    private boolean autenticar(HttpServletRequest request) {
+    	// TODO Auto-generated method stub
+    	return false;
+    }
+    
+
+    public void doGet (HttpServletRequest request, HttpServletResponse response){
+		processar(request, response);
+	}
+
+	public void doPost (HttpServletRequest request, HttpServletResponse response){
+		processar(request, response);
+	}
+	
+	public void processar(HttpServletRequest request, HttpServletResponse response){
+
+		try{
+
+			/* recuperando acao */			
+			String acao = request.getParameter("acao");
+			String url = null;
+			
+			
+			if(acao.equals("cadastrar")){
+				if (ServletHotel.DEBUG){ System.out.println("acao = cadastrar"); } 
+				if (this.cadastrarUsuario(request)){
+					url = "home.jsp";					
+				}
+				
+			} else if (acao.equals("login")){
+				if (ServletHotel.DEBUG){ System.out.println("acao = login"); } 
+				if (this.autenticar(request)){
+					url = "home.jsp";					
+				}
+				
+			} else {
+				/* nenhuma acao foi definida */				
+				/* selecionando pagina para o dispatcher */				
+				url="erro.jsp";
+			}
+
+			RequestDispatcher dispatcher = request.getRequestDispatcher("../"+url);
+			dispatcher.forward(request, response);
+
+		}catch(Exception e){
+			e.printStackTrace();
+		}
+
+	}
+
+	
+	
+}
---
+++ projeto_parte2/singaporeHotel/target/singaporeHotel/js/booking_controller.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+	$("#bookingform").on('submit_booking', function() {
+		var new_booking = new Booking();
+		new_booking.setDateCheckIn($("#tfdataentrada").val());
+		new_booking.setDateCheckOut($("#tfdatasaida").val());
+		new_booking.setNumberOfAdults(parseInt($("#numberOfAdults").val()));
+		new_booking.setNumberOfBabies(parseInt($("#numberOfBabies").val()));
+		new_booking.setNumberOfChildren(parseInt($("#numberOfChildren").val()));
+		new_booking.saveData();
+		if(new_booking.isValidBooking())
+			alert("Sua reserva foi efetuada com sucesso.\n");
+		else
+			alert("Reserva não efetuada. Tente novamente mais tarde");
+	});
+});
