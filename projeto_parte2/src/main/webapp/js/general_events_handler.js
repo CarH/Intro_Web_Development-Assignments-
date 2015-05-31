083a9a735e02bb6297e44cf4e3f25c0f4a6d9b07
CarH <carlosh.usp@gmail.com>
2015-05-31 16:59:58 -0300

    remocao do dir

--- projeto_parte2/src/main/webapp/js/general_events_handler.js
+++
@@ -1,9 +0,0 @@
-// Esse CSS contém os eventos mais gerais, associados com várias paginas,
-// eventos associados ao menu
-$(document).ready(function  () {
-
-	$("#registerbtn").click(function(){
-		window.location.href = "registration.html";
-	});
-
-});
